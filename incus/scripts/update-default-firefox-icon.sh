75da3121b30a6d939098bc15c1e35c74250da473
Mark Dechiaro <mdechiaro@users.noreply.github.com>
2025-11-17 09:53:53 -0600

    Fix script to include quotes

--- incus/scripts/update-default-firefox-icon.sh
+++ incus/scripts/update-default-firefox-icon.sh
@@ -14,7 +14,11 @@ fi
 mkdir -p /usr/local/share/applications/
 cp /usr/share/applications/org.mozilla.firefox.desktop \
   /usr/local/share/applications/
-sed -i "s/^Exec=firefox/Exec=incus exec ${1} -- su -l user -c firefox/g" \
+sed -i \
+  -e "s/^Exec=firefox %u/Exec=incus exec ${1} -- su -l user -c \"firefox %u\"/g" \
+  -e "s/^Exec=firefox --new-window %u/Exec=incus exec ${1} -- su -l user -c \"firefox --new-window %u\"/g" \
+  -e "s/^Exec=firefox --private-window %u/Exec=incus exec ${1} -- su -l user -c \"firefox --private-window %u\"/g" \
+  -e "s/^Exec=firefox --ProfileManager/Exec=incus exec ${1} -- su -l user -c \"firefox --ProfileManager\"/g" \
   /usr/local/share/applications/org.mozilla.firefox.desktop
 
 update-desktop-database
