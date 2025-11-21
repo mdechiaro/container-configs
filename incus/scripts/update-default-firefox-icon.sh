b9720463342e10a5f2235c4753dc25df91371413
Mark Dechiaro <mdechiaro@users.noreply.github.com>
2025-11-21 07:25:06 -0600

    Fix script to not require root privileges

--- incus/scripts/update-default-firefox-icon.sh
+++ incus/scripts/update-default-firefox-icon.sh
@@ -1,24 +1,28 @@
 #!/usr/bin/env bash
 # updates default firefox gnome desktop icon to use container instead
 
-if [[ $EUID -ne 0 ]]; then
-  echo "script needs to be run as root."
+if [[ $EUID == 0 ]]; then
+  echo "script does not need root privileges."
   exit 1
 fi
 
 if [[ $# != 1 ]]; then
-  echo "Usage: ${0} <container_name>"
+  echo "Usage: ${0} name"
   exit 1
 fi
 
-mkdir -p /usr/local/share/applications/
+if ! [ -d ${HOME}/.local/share/applications ]; then
+  mkdir -p ${HOME}/.local/share/applications
+fi
+
 cp /usr/share/applications/org.mozilla.firefox.desktop \
-  /usr/local/share/applications/
+  ${HOME}/.local/share/applications
+
 sed -i \
   -e "s/^Exec=firefox %u/Exec=incus exec ${1} -- su -l user -c \"firefox %u\"/g" \
   -e "s/^Exec=firefox --new-window %u/Exec=incus exec ${1} -- su -l user -c \"firefox --new-window %u\"/g" \
   -e "s/^Exec=firefox --private-window %u/Exec=incus exec ${1} -- su -l user -c \"firefox --private-window %u\"/g" \
   -e "s/^Exec=firefox --ProfileManager/Exec=incus exec ${1} -- su -l user -c \"firefox --ProfileManager\"/g" \
-  /usr/local/share/applications/org.mozilla.firefox.desktop
+  ${HOME}/.local/share/applications/org.mozilla.firefox.desktop
 
-update-desktop-database
+update-desktop-database -q
