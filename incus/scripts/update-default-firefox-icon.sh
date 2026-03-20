4152c2ebee2027809dea4f85892706f315544a16
Mark Dechiaro <mdechiaro@users.noreply.github.com>
2026-03-20 12:28:47 -0500

    Fix firefox container setup and scripts

--- incus/scripts/update-default-firefox-icon.sh
+++ incus/scripts/update-default-firefox-icon.sh
@@ -19,10 +19,10 @@ cp /usr/share/applications/org.mozilla.firefox.desktop \
   ${HOME}/.local/share/applications
 
 sed -i \
-  -e "s/^Exec=firefox %u/Exec=incus exec ${1} -- su -l user -c \"firefox %u\"/g" \
-  -e "s/^Exec=firefox --new-window %u/Exec=incus exec ${1} -- su -l user -c \"firefox --new-window %u\"/g" \
-  -e "s/^Exec=firefox --private-window %u/Exec=incus exec ${1} -- su -l user -c \"firefox --private-window %u\"/g" \
-  -e "s/^Exec=firefox --ProfileManager/Exec=incus exec ${1} -- su -l user -c \"firefox --ProfileManager\"/g" \
+  -e "s/^Exec=firefox %u/Exec=incus exec ${1} -- su user -c \"firefox %u\"/g" \
+  -e "s/^Exec=firefox --new-window %u/Exec=incus exec ${1} -- su user -c \"firefox --new-window %u\"/g" \
+  -e "s/^Exec=firefox --private-window %u/Exec=incus exec ${1} -- su user -c \"firefox --private-window %u\"/g" \
+  -e "s/^Exec=firefox --ProfileManager/Exec=incus exec ${1} -- su user -c \"firefox --ProfileManager\"/g" \
   ${HOME}/.local/share/applications/org.mozilla.firefox.desktop
 
 update-desktop-database -q
